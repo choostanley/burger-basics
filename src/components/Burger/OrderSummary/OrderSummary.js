6d3487becf7e33399df98288522029f39acef6d2
Stanley Choo <stanley@goget.my>
2019-03-23 19:33:29 +0800

    added component lifecycle to modal and ordersummary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-	const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+
+	//this can be a functional component
+	componentWillUpdate() {
+		console.log('[OrderSummary] Will update!');
+	}	
+
+	render () {
+
+		const ingredientSummary = Object.keys(this.props.ingredients)
 		.map(igKey => {
 			return (
 				<li key={igKey}>
-					<span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+					<span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
 				</li>
 				);
 		});
 
-	return (
-		<Aux>
-			<h3>Your order</h3>
-			<p>Ingredients:</p>
-			<p>Total price: {props.price.toFixed(2)}</p>
-			<ul>
-				{ingredientSummary}
-			</ul>
-			<p>Checkout?</p>
-			<Button btntype="Danger" clicked={props.purchaseCancelled}>Cancel</Button>
-			<Button btntype="Success" clicked={props.purchaseContinued}>Continue</Button>
-		</Aux>
-		);
-};
+		return (
+			<Aux>
+				<h3>Your order</h3>
+				<p>Ingredients:</p>
+				<p>Total price: {this.props.price.toFixed(2)}</p>
+				<ul>
+					{ingredientSummary}
+				</ul>
+				<p>Checkout?</p>
+				<Button btntype="Danger" clicked={this.props.purchaseCancelled}>Cancel</Button>
+				<Button btntype="Success" clicked={this.props.purchaseContinued}>Continue</Button>
+			</Aux>
+			);
+	}
+}
 
-export default orderSummary;
+export default OrderSummary;
